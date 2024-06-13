74b854a501560615ba410c921bc94b213ac08bca
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-06-13 16:05:38 +0300

    Submitting empty batch

--- test/test.hs
+++ test/test.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import           Control.Exception (SomeException, try)
+import qualified Data.Vector       as V
 import           System.IO.BlockIO
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -11,12 +12,19 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "test"
     [ testCase "example_initClose" example_initClose
+    , testCase "example_initEmptyClose" example_initEmptyClose
     , testCase "example_closeIsIdempotent" example_closeIsIdempotent
     ]
 
 example_initClose :: Assertion
 example_initClose = do
     ctx <- initIOCtx defaultIOCtxParams
+    closeIOCtx ctx
+
+example_initEmptyClose :: Assertion
+example_initEmptyClose = do
+    ctx <- initIOCtx defaultIOCtxParams
+    _ <- submitIO ctx V.empty
     closeIOCtx ctx
 
 example_closeIsIdempotent :: Assertion
