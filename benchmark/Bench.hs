bc3bf85742656ec9c2bf8aede588e19386aa1899
Duncan Coutts <duncan@well-typed.com>
2024-09-18 18:05:15 +0100

    Adjust high level benchmark approach to reduce allocations
    Previously we generated a large list of a random permutation of all the
    4k disk blocks in the input file, and then broke that up into groups of
    32 blocks. We used forConcurrently_ over the groups which starts one
    thread per group.
    
    This approach did excessive work and allocated a lot. For starters the
    random permutation is expensive in time and space. It needed to track a
    set of remaining choices. And it had to allocate the whole list because
    the forConcurrently_ forces the list (of groups). And a thread per 32
    blocks is a lot of threads once one uses large files. For our example
    target file of 262144 blocks, this amounts to 8192 threads. Each one has
    a stack, so it's a lot of allocations in total.
    
    The new approach is:
    1. Use random block indexes rather than a permutation. This leads to
       duplicates but is much cheaper because it needs little state. The
       best solution to the duplicates will be to use O_DIRECT to avoid
       caching effects.
    2. Only use a small number of threads and have each one submit a series
       of batches of work (still of size 32).
    
    This uses much less state. Then in addition, we use a lower level
    approach to generatng the random block indexes to reduce heap
    allocations.
    
    The new allocation results for the high level benchmark are:
    
    Allocated total: 57904976
    Allocated per:   221
    
    That is, 221 bytes per IO op. This is without any changes yet to the
    actual library, just the benchmark.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -7,12 +7,15 @@ module Main (main) where
 import Data.Primitive
 import qualified Data.Set as Set
 import Control.Monad
+import Control.Monad.Primitive (RealWorld)
+import Control.Monad.ST (ST)
 import Control.Exception
-import Control.Concurrent.Async
+import Control.Concurrent.Async as Async
 
 import Foreign
 import System.Posix.IO
 import System.Posix.Files
+import System.Posix.Types as Posix
 
 import System.Random as Random
 import System.Environment
@@ -25,6 +28,7 @@ import System.IO.BlockIO
 import System.IO.BlockIO.URing hiding (submitIO)
 import qualified System.IO.BlockIO.URing as URing
 import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 
 main :: IO ()
 main = do
@@ -105,19 +109,60 @@ main_highlevel filename = do
   let size      = fileSize status
       lastBlock :: Int
       lastBlock = fromIntegral (size `div` 4096 - 1)
-      nbufs     = ioctxConcurrencyLimit params
-      params    = defaultIOCtxParams
-      blocks    = V.fromList $ zip [0..] (randomPermute rng [0..lastBlock])
-      totalOps  = lastBlock + 1
-  bracket (initIOCtx params) closeIOCtx $ \ioctx -> do
-    buf <- newAlignedPinnedByteArray (4096 * nbufs) 4096
+      params    = IOCtxParams {
+                    ioctxBatchSizeLimit   = 64,
+                    ioctxConcurrencyLimit = 64 * 4
+                  }
+      ntasks    = 4
+      batchsz   = 32
+      nbatches  = lastBlock `div` (ntasks * batchsz) -- batches per task
+      totalOps  = nbatches * batchsz * ntasks
+  bracket (initIOCtx params) closeIOCtx $ \ioctx ->
+    withReport totalOps $ do
+      tasks <-
+        forRngSplitM ntasks rng $ \ !rng_task ->
+          Async.async $ do
+            buf <- newAlignedPinnedByteArray (4096 * batchsz) 4096
+            forRngSplitM_ nbatches rng_task $ \ !rng_batch ->
+              submitIO ioctx $
+                generateIOOpsBatch fd buf lastBlock batchsz rng_batch
+      _ <- Async.waitAnyCancel tasks
+      return ()
 
-    withReport totalOps $
-      forConcurrently_ (groupsOfN 32 blocks) $ \batch ->
-        submitIO ioctx $ flip fmap batch $ \ (i, block) ->
-          let bufOff  = (i `mod` nbufs) * 4096
-              blockoff = fromIntegral (block * 4096)
-          in  IOOpRead fd blockoff buf bufOff 4096
+generateIOOpsBatch :: Posix.Fd
+                   -> MutableByteArray RealWorld
+                   -> Int
+                   -> Int
+                   -> Random.StdGen
+                   -> V.Vector (IOOp IO)
+generateIOOpsBatch !fd !buf !lastBlock !size !rng0 =
+    V.create $ do
+      v <- VM.new size
+      go v rng0 0
+      return v
+  where
+    go :: V.MVector s (IOOp IO) -> Random.StdGen -> Int -> ST s ()
+    go !_ !_   !i | i == size = return ()
+    go !v !rng !i = do
+      let (!block, !rng') = Random.uniformR (0, lastBlock) rng
+          !bufOff   = i * 4096
+          !blockoff = fromIntegral (block * 4096)
+      VM.unsafeWrite v i $! IOOpRead fd blockoff buf bufOff 4096
+      go v rng' (i+1)
+
+forRngSplitM_ :: Monad m => Int -> Random.StdGen -> (Random.StdGen -> m a) -> m ()
+forRngSplitM_ n rng0 action = go n rng0
+  where
+    go 0  !_   = return ()
+    go !i !rng = let (!rng', !rng'') = Random.split rng
+                  in action rng' >> go (i-1) rng''
+
+forRngSplitM :: Monad m => Int -> Random.StdGen -> (Random.StdGen -> m a) -> m [a]
+forRngSplitM n rng0 action = go [] n rng0
+  where
+    go acc 0  !_   = return (reverse acc)
+    go acc !i !rng = let (!rng', !rng'') = Random.split rng
+                      in action rng' >>= \x -> go (x:acc) (i-1) rng''
 
 withReport :: Int -> IO () -> IO ()
 withReport totalOps action = do
@@ -144,10 +189,6 @@ report beforeTime afterTime beforeRTS afterRTS totalOps = do
     iops, apio :: Int
     iops = round (fromIntegral totalOps / realToFrac elapsed :: Double)
     apio = round (fromIntegral allocated / realToFrac totalOps :: Double)
-
-groupsOfN :: Int -> V.Vector a -> [V.Vector a]
-groupsOfN n xs | V.null xs = []
-               | otherwise = V.take n xs : groupsOfN n (V.drop n xs)
 
 randomPermute :: Ord a => StdGen -> [a] -> [a]
 randomPermute rng0 xs0 =
