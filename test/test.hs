3c20c7136acb5b0f1a50e465e43f0e36c64f8576
Joris Dral <joris@well-typed.com>
2025-05-01 11:46:33 +0200

    Test the validation of `IOCtxParams`
    This shows a small bug where the configured SQ ring size is rounded up
    automatically by `liburing`
    
    ```
    ❯ cabal run test -- -p "prop_ValidIOCtxParams"
    test
      prop_ValidIOCtxParams: FAIL
        *** Failed! Falsified (after 20 tests and 2 shrinks):
        IOCtxParams {ioctxBatchSizeLimit = 3, ioctxConcurrencyLimit = 3}
        Unknown exception: user error (setupURing initialisation failure: unexected SQ ring size (3,4))
        Use --quickcheck-replay="(SMGen 7259989759507308958 11179066890466316627,19)" to reproduce.
    
    1 out of 1 tests failed (0.00s)
    ```

--- test/test.hs
+++ test/test.hs
@@ -1,15 +1,22 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns     #-}
+
 module Main (main) where
 
-import           Control.Exception        (SomeException, try)
+import           Control.Exception        (Exception (displayException),
+                                           IOException, SomeException, try)
+import           Data.List                (isPrefixOf)
 import qualified Data.Primitive.ByteArray as P
 import qualified Data.Vector              as V
+import           GHC.IO.Exception         (IOException (ioe_description, ioe_location))
 import           GHC.IO.FD                (FD (..))
 import           GHC.IO.Handle.FD         (handleToFd)
 import           System.IO
 import           System.IO.BlockIO
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
 
 main :: IO ()
 main = defaultMain tests
@@ -22,6 +29,7 @@ tests = testGroup "test"
     , testCase "example_initReadClose 200" $ example_initReadClose 200
     , testCase "example_initEmptyClose" example_initEmptyClose
     , testCase "example_closeIsIdempotent" example_closeIsIdempotent
+    , testProperty "prop_ValidIOCtxParams" prop_ValidIOCtxParams
     ]
 
 example_initClose :: Assertion
@@ -56,3 +64,76 @@ example_closeIsIdempotent = do
         assertFailure ("Close on a closed context threw an error : " <> show (e :: SomeException))
       Right () ->
         pure ()
+
+{-------------------------------------------------------------------------------
+  Valid IOCtxParams
+-------------------------------------------------------------------------------}
+
+-- | We test @validateIOCtxParams@ through 'withIOCtx'. If any params slip
+-- through the cracks, then the call to @setupURing@ inside 'withIOCtx' should
+-- throw an unexpected exception, which causes the property to fail.
+prop_ValidIOCtxParams :: IOCtxParams -> Property
+prop_ValidIOCtxParams params@IOCtxParams{..} =
+    checkCoverage $
+    coverTable "Result" [("Success", 5)] $
+    ioProperty $ do
+      eith <- try @IOException $ withIOCtx params $ \ctx -> pure ()
+      pure $ case eith of
+        Left e
+          |  "IOCtxParams are invalid" `isPrefixOf` ioe_location e
+            &&
+            not (
+              inBoundsExcl ioctxBatchSizeLimit batchSizeLimitBoundsExcl &&
+              inBoundsExcl ioctxConcurrencyLimit (concurrencyLimitBoundsExcl (ioctxBatchSizeLimit - 1))
+            )
+          -> tabulate "Result" [displayException e] True
+          | otherwise
+          -> counterexample ("Unknown exception: " ++ displayException e) False
+        Right () -> tabulate "Result" ["Success"] True
+  where
+    inBoundsExcl x (lb, ub) = lb < x && x < ub
+
+    batchSizeLimitBoundsExcl = (0, 2^(15 :: Int))
+    concurrencyLimitBoundsExcl batchSizeLimit =  (batchSizeLimit, 2^(16::Int))
+
+instance Arbitrary IOCtxParams where
+  arbitrary = IOCtxParams <$> genLimit <*> genLimit
+  shrink (IOCtxParams a b) =
+      [ IOCtxParams a' b' | (a', b') <- liftShrink2 shrinkLimit shrinkLimit (a, b) ]
+
+genLimit :: Gen Int
+genLimit = frequency [
+      -- Generate powers of 2 to hit the upper bounds on the batch size limit
+      -- and concurrency limit
+      (10, genPowerOf2)
+      -- Get some coverage of the whole range between the lower and upper
+      -- bounds on the batch size limit and concurrency limit.
+    , (10, chooseNonNegativeInt)
+      -- Otherwise, generate integers like normal
+    , (10, arbitrary)
+    ]
+
+shrinkLimit :: Int -> [Int]
+shrinkLimit x = shrinkPowerOf2 x ++ shrinkNonNegativeInt x ++ shrink x
+
+genPowerOf2 :: Gen Int
+genPowerOf2 = do
+    i <- chooseInt (minExponent, maxExponent)
+    pure (2^i)
+
+shrinkPowerOf2 :: Int -> [Int]
+shrinkPowerOf2 x = [x' | i <- [ minExponent .. maxExponent ], let x' = 2^i, x' < x]
+
+minExponent, maxExponent :: Int
+minExponent = 0
+maxExponent = 20
+
+chooseNonNegativeInt :: Gen Int
+chooseNonNegativeInt = chooseInt (lb, ub)
+
+shrinkNonNegativeInt :: Int -> [Int]
+shrinkNonNegativeInt x = [ x' | NonNegative x' <- shrink (NonNegative x) ]
+
+lb, ub :: Int
+lb = 0
+ub = 2^maxExponent
